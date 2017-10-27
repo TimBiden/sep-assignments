95c3534786fc9675113263e34de8733855dd42d3
TimBiden <timbiden@gmail.com>
2017-10-27 12:11:39 -0700

    Line Completed.

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -5,30 +5,39 @@ class Line
   attr_accessor :members
 
   def initialize
-    self.members = []
+    @members = []
   end
 
   def join(person)
+    @members.push person
   end
 
   def leave(person)
+    @members.delete(person)
   end
 
   def front
+    @members[0]
   end
 
   def middle
+    @members[@members.length / 2]
   end
 
   def back
+    @members[-1]
   end
 
   def search(person)
+    if index(person)
+      @members[index(person)]
+    end
   end
 
   private
 
   def index(person)
+    @members.index(person)
   end
 
 end
