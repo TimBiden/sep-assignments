9e83f849ce20c181faf1b624149001621f5fac68
TimBiden <timbiden@gmail.com>
2017-10-30 08:46:57 -0700

    Revert "My turn to drive."
    This reverts commit 781a5c29b7741499f443f2c1fde012fa23259eb5.

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -6,29 +6,18 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
-    self.width = width
-    self.height = height
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
-    if inbounds(x, y)
-
-    end
   end
 
   def at(x, y)
-    self.x = x
-    self.y = y
   end
 
   private
 
   def inbounds(x, y)
-    if x > self.width && x > 0 && y > self.height && y > 0
-      true
-    else
-      false
-    end
   end
+
 end
