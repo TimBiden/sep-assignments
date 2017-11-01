5a575d8ace0414a4d18dfb00bdc197a4c3ba3884
TimBiden <timbiden@gmail.com>
2017-11-01 16:46:44 -0700

    Last benchmark not passing.

--- 01-data-structures/03-linked-lists/benchmarking.rb
+++ 01-data-structures/03-linked-lists/benchmarking.rb
@@ -1,3 +1,45 @@
-require 'Benchmark'
-require_relative 'linked_list'
+require 'benchmark'
 require_relative 'node'
+require_relative 'linked_list'
+
+n = 10_000
+array = []
+llist = LinkedList.new
+
+Benchmark.bm(1) do |x|
+  x.report('LList Add:') do
+    for i in 1..n do
+      node = Node.new(i)
+      llist.add_to_tail(node)
+    end
+  end
+
+  x.report('Array Add:') do
+    for i in 1..n do
+      array.push(i)
+    end
+  end
+
+  puts ' '
+
+  x.report('LList Find:') do
+    temp = llist.head
+    for i in 1..5000
+      temp = temp.next
+    end
+  end
+
+  x.report('Array Find:') do
+    array[5000]
+  end
+
+  puts ' '
+
+  x.report('LList Del:') do
+    llist.delete(5000)
+  end
+
+  x.report('Array Del:') do
+    array.delete_at(array.index(5000))
+  end
+end
