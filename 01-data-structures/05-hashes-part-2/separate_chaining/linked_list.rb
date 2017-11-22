6e2be55d77341dda9f12505d34751b39693eac6e
TimBiden <timbiden@gmail.com>
2017-11-22 15:50:48 -0800

    Bingo!!!

--- 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
@@ -1,4 +1,3 @@
-require_relative 'benchmarking'
 require_relative 'node'
 
 class LinkedList
--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -5,12 +5,37 @@ class SeparateChaining
 
   def initialize(size)
     @max_load_factor = 0.7
+    @items = Array.new(size)
+    @numItems = 0
   end
 
   def []=(key, value)
+    newItem = Node.new(key, value)
+    newIndex = index(key, size)
+
+    if @items[newIndex].nil?
+      @items[newIndex] = LinkedList.new
+      @items[newIndex].add_to_tail(newItem)
+    else
+      @items[newIndex].add_to_tail(newItem)
+    end
+
+    @numItems += 1
   end
 
   def [](key)
+    newIndex = index(key, size)
+    if @items[newIndex]
+      data = @items[newIndex].head
+      while data
+        if data.key == key
+          return data.value
+        else
+          data = data.next
+        end
+      end
+    end
+    @items[newIndex].value
   end
 
   # Returns a unique, deterministically reproducible index into an array
@@ -22,14 +47,38 @@ class SeparateChaining
 
   # Calculate the current load factor
   def load_factor
-    
+    if @numItems.fdiv(size) > @max_load_factor
+      resize
+    end
+
+    @numItems.fdiv(size)
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.length
   end
 
   # Resize the hash
   def resize
+    newLength = (@items.length * 2)
+    oldArray = @items
+    @items = Array.new(newLength)
+
+    for i in 0..oldArray.length do
+      if oldArray[i]
+        dataMove = oldArray[i].head
+        newIndex = index(dataMove.key, size)
+        if @items[newIndex]
+          @items[newIndex].add_to_tail(dataMove)
+        else
+          @items[newIndex] = LinkedList.new
+          @items[newIndex].add_to_front(dataMove)
+        end
+      end
+      i += 1
+    end
+
+    @items
   end
 end
