b50b1448be9758981b07d4a87c1ee3dd652ccb4e
TimBiden <timbiden@gmail.com>
2017-11-28 12:17:13 -0800

    Insert created.
    May need to be edited.

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -1,21 +1,33 @@
 require_relative 'node'
 
 class BinarySearchTree
-
   def initialize(root)
+    @root = root
   end
 
   def insert(root, node)
+    if root.rating > node.rating
+      if root.left
+        insert(root.left, node)
+      else
+        root.left = node
+      end
+    else
+      if root.right
+        insert(root.right, node)
+      else
+        root.right = node
+      end
+    end
   end
 
   # Recursive Depth First Search
   def find(root, data)
+    
   end
 
-  def delete(root, data)
-  end
+  def delete(root, data); end
 
   # Recursive Breadth First Search
-  def printf(children=nil)
-  end
+  def printf(children = nil); end
 end
