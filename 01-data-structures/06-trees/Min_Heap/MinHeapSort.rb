c6a79a0b2ae52082ea8148d16e41d113d18199ed
TimBiden <timbiden@gmail.com>
2017-12-07 07:21:04 -0800

    Basic insert and sort works.

--- 01-data-structures/06-trees/Min_Heap/MinHeapSort.rb
+++ 01-data-structures/06-trees/Min_Heap/MinHeapSort.rb
@@ -5,100 +5,45 @@ class MinBinaryHeap
 
   def initialize(root)
     @root = root
-    @bottomChildren = 0
-    @bottomRow = 2
   end
 
   def insert(root, node)
-    # If root rating > node rating, swap and recurse.
+    return false if root.nil?
 
-    if root.rating > node.rating
-      # Swap values
-      # puts "Time to swap!"
-      # puts "node is #{node.title}"
-      # puts "root is #{root.title}"
-      @root = node
-      node = root
-      # puts "node is #{node.title}"
-      # puts "@root is #{@root.title}"
-
-      # Recursion
-      insert(@root, node)
-
-    elsif root.rating < node.rating
+    if node.rating <= root.rating
       if root.left.nil?
-        # Insert node into left if open
-        node.parent = root
         root.left = node
-        puts ' '
-        puts "Top"
-        puts ' '
-        puts "root.title = #{root.title}"
-        puts "root.rating = #{root.rating}"
-        puts "root.left.title = #{root.left.title}"
-        puts "root.left.rating = #{root.left.rating}"
-        @bottomChildren += 1
-      elsif root.right.nil?
-        # Insert node into right if open
-        puts "Insert into Right!"
-        node.parent = root
-        root.right = node
-        @bottomChildren += 1
-      # else
-      #   bottomHalf = @bottomRow / 2
-      #   if @bottomChildren < bottomHalf
-      #     insert(root.left, node)
-      #   elsif @bottomChildren >= bottomHalf && @bottomChildren < @bottomRow
-      #     insert(root.right, node)
-      #   elsif @bottomChildren == @bottomRow
-      #     @bottomChildren = 0
-      #     @bottomRow *= 2
-      #     insert(root.left, node)
-      #   else
-      #     puts "D'oh! Bottom Half issue."
-      #   end
+        heap_sort(root)
+        return
+      else
+        insert(root.left, node)
       end
-    else
-      puts 'Are the ratings equal?'
+    elsif node.rating > root.rating
+      if root.right.nil?
+        root.right = node
+        heap_sort(root)
+        return
+      else
+        insert(root.right, node)
+      end
     end
-    puts ' '
-    puts "Bottom"
-    puts ' '
-    puts "root.title = #{root.title}"
-    puts "root.rating = #{root.rating}"
-    puts "root.left.title = #{root.left.title}"
-    puts "root.left.rating = #{root.left.rating}"
   end
 
-  # def find(root, name)
-  #   findArray = [root]
-  #
-  #   findArray.each do |i|
-  #     if i.title == name
-  #       return i
-  #     else
-  #       findArray.push(i.right) unless i.right.nil?
-  #       findArray.push(i.left) unless i.left.nil?
-  #     end
-  #   end
-  # end
-  #
-  # def delete(root, name)
-  #   if name.nil?
-  #     nil
-  #   else
-  #     toDelete = find(root, name)
-  #     toDelete.rating = nil
-  #     toDelete.title = nil
-  #   end
-  # end
-  #
-  # def printf(children = nil)
-  #   toPrint = [@root]
-  #   toPrint.flatten!
-  #
-  #   toPrint.each do |i|
-  #     puts "#{i.title}: #{i.rating}"
-  #   end
-  # end
+  def heap_sort(root)
+    if root.left && root.right && root.left.rating > root.right.rating
+      root.left.title, root.right.title = root.right.title, root.left.title
+      root.left.rating, root.right.rating = root.right.rating, root.left.rating
+      heap_sort(root)
+    elsif root.left && root.rating > root.left.rating
+      root.left.title, root.title = root.title, root.left.title
+      root.left.rating, root.rating = root.rating, root.left.rating
+      heap_sort(@root)
+    elsif root.right && root.rating > root.right.rating
+      root.right.title, root.title = root.title, root.right.title
+      root.right.rating, root.rating = root.rating, root.right.rating
+      heap_sort(@root)
+    else
+      puts "Houston, we have a problem."
+    end
+  end
 end
