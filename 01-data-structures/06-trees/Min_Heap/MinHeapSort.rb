500abe155745b83fbd2d1dd69f7acb56bbf91127
TimBiden <timbiden@gmail.com>
2017-12-13 07:39:25 -0800

    Revert "First `delete` test works!!!"
    This reverts commit 5351e11f7f010b1c57e1897e2461eabb16d4ba87.

--- 01-data-structures/06-trees/Min_Heap/MinHeapSort.rb
+++ 01-data-structures/06-trees/Min_Heap/MinHeapSort.rb
@@ -30,68 +30,19 @@ class MinBinaryHeap
   end
 
   def heap_sort(root)
-    # This should work.
-    if root.rating && root.rating == 'delete'
-      if root.right && root.left && root.right.rating >= root.left.rating
-        root_left_to_root(root)
-      elsif root.right && root.left && root.right.rating < root.left.rating
-        root_right_to_root(root)
-      else
-        root.left && root.right.rating >= root.left.rating
-      end
-    end
-
-    # Check this logic!
-    if root.left && root.left.rating == 'delete'
-      if root.right.rating >= root.left.rating
-        root_left_to_root(root)
-      else
-        root_right_to_root(root)
-      end
-    elsif root.right && root.right.rating == 'delete'
-      if root.left && root.right.rating >= root.left.rating
-        root_left_to_root(root)
-      else
-        heap_sort(root.right) # <---- Pay attention here!
-      end
-    elsif root.left && root.right && root.left.rating > root.right.rating
-      left_to_right(root)
+    if root.left && root.right && root.left.rating > root.right.rating
+      root.left.title, root.right.title = root.right.title, root.left.title
+      root.left.rating, root.right.rating = root.right.rating, root.left.rating
+      heap_sort(root)
     elsif root.left && root.rating > root.left.rating
-      root_left_to_root(root)
+      root.left.title, root.title = root.title, root.left.title
+      root.left.rating, root.rating = root.rating, root.left.rating
+      heap_sort(@root)
     elsif root.right && root.rating > root.right.rating
-      root_right_to_root(root)
+      root.right.title, root.title = root.title, root.right.title
+      root.right.rating, root.rating = root.rating, root.right.rating
+      heap_sort(@root)
     end
-  end
-
-  def left_to_right(root)
-    root.left.title, root.right.title = root.right.title, root.left.title
-    root.left.rating, root.right.rating = root.right.rating, root.left.rating
-    heap_sort(root)
-  end
-
-  def root_left_to_root(root)
-    root.left.title, root.title = root.title, root.left.title
-    root.left.rating, root.rating = root.rating, root.left.rating
-    heap_sort(@root)
-  end
-
-  def root_right_to_root(root)
-    root.right.title, root.title = root.title, root.right.title
-    root.right.rating, root.rating = root.rating, root.right.rating
-    heap_sort(@root)
-  end
-
-  def delete(root, name)
-    return nil if root.nil? || name.nil?
-    node = find(root, name)
-
-    node.title = 'delete'
-    node.rating = 'delete'
-    heap_sort(root)
-
-    deleteNode = find(root, "delete")
-    deleteNode.title = nil
-    deleteNode.rating = nil
   end
 
   def find(root, name)
@@ -106,5 +57,12 @@ class MinBinaryHeap
         findArray.push(i.left) unless i.left.nil?
       end
     end
+  end
+
+  def delete(root, name)
+    return nil if root.nil? || name.nil?
+    item = self.find(root, name)
+      item.title = nil
+      item.rating = nil
   end
 end
