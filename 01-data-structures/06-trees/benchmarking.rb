2f81be5c6d6f9f1377d6b9d03870dc0ca00ee042
TimBiden <timbiden@gmail.com>
2017-12-19 16:45:09 -0800

    Error in test.
    Undefined local variable or method `title'

--- 01-data-structures/06-trees/benchmarking.rb
+++ 01-data-structures/06-trees/benchmarking.rb
@@ -1,8 +1,28 @@
-require_relative 'binary_tree/binary_search_tree.rb'
-require_relative 'binary_tree/node.rb'
-require_relative 'Min_heap/MinHeapRSpec.rb'
-require_relative 'Min_heap/node.rb'
-require 'benchmark'
+require_relative 'Min_Heap/MinHeapSort.rb'
+require_relative 'Min_Heap/node.rb'
+# require_relative 'binary_tree/binary_search_tree.rb'
+# require_relative 'binary_tree/node.rb'
+# require 'benchmark'
 
-size = 100000
-find = 50000
+
+size = 100
+find = 50
+
+@array = []
+for i in 1..size do
+  @array.push({title =>i.to_s, rating => i})
+end
+
+@array.shuffle!
+
+def callMinHeap
+  puts "Calling the heap."
+
+  heap = MinBinaryHeap.new(@array[0])
+
+  @array.each do |node|
+		heap.insert(heap.root, node)
+	end
+end
+
+callMinHeap()
