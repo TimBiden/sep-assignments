6a30c25904ce1ea2d94ed8df6731d141fc153f43
TimBiden <timbiden@gmail.com>
2017-12-21 12:08:38 -0800

    undefined method `insert'

--- 01-data-structures/06-trees/benchmarking.rb
+++ 01-data-structures/06-trees/benchmarking.rb
@@ -2,31 +2,34 @@ require_relative 'Min_Heap/MinHeapSort.rb'
 require_relative 'Min_Heap/node.rb'
 # require_relative 'binary_tree/binary_search_tree.rb'
 # require_relative 'binary_tree/node.rb'
-# require 'benchmark'
+require 'benchmark'
 
-
-size = 100
-find = 50
-@array = []
-
-for i in 1..size do
-  @array.push(i)
-end
-
-@array.shuffle!
-
-def callMinHeap
-  puts "Calling the heap."
-
-  heapArray = @array.each do |i|
-    Node.new(i.to_s, i)
-  end
-
-  heap = MinBinaryHeap.new(heapArray[0])
-
-  heapArray.each do |j|
-		heap.insert(heap, j)
+def min_heap(shuffledArray)
+	nodeArray = shuffledArray.each do |num|
+		Node.new(num.to_s, num)
 	end
+
+	nodeVal = shuffledArray[0]
+	puts "nodeVal = #{nodeVal}"
+	heap = Node.new(nodeVal.to_s, nodeVal)
+
+	shuffledArray.each do |i|
+	  heap.insert(heap, Node.new(i.to_s, i))
+	end
+
+	return heap
 end
 
-callMinHeap()
+# Example Code
+Benchmark.bm do |x|
+  # find = 50
+  size = 100
+  shuffledArray = []
+
+  for i in 1..size do
+    shuffledArray.push(i)
+  end
+  shuffledArray.shuffle!
+
+	x.report('inserting shuffled shuffledArray into min heap') {new_heap = min_heap(shuffledArray)}
+end
