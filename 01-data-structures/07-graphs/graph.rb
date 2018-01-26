97be4ebb1838413ba40b1674370ddcd85854865c
TimBiden <timbiden@gmail.com>
2018-01-26 13:58:34 -0800

    Movie Data method

--- 01-data-structures/07-graphs/graph.rb
+++ 01-data-structures/07-graphs/graph.rb
@@ -35,54 +35,49 @@ def add_edges(node1, node2)
 end
 
 def movie_iteration(movie_to_iterate)
-    movie_to_iterate.each do|movie, actors|
-        actors.each do|actor|
+    movie_to_iterate.each do |movie, actors|
+        actors.each do |actor|
             add_edges(movie, actor)
         end
     end
 end
 
-def actor_iteration(actor_to_iterate)
-    movie_to_iterate.each do|actor, movies|
-        movies.each do|movie|
+def actor_iteration(_actor_to_iterate)
+    movie_to_iterate.each do |actor, movies|
+        movies.each do |movie|
             add_edges(actor, movie)
         end
     end
 end
 
-# Actor nodes
-kevin_bacon = Node.new('Kevin Bacon')
-marisa_tomei = Node.new('Marisa Tomei')
-joey_king = Node.new('Joey King')
-john_carroll_lynch = Node.new('John Carroll Lynch')
+def movie_data
+    # Actor nodes
+    kevin_bacon = Node.new('Kevin Bacon')
+    marisa_tomei = Node.new('Marisa Tomei')
+    joey_king = Node.new('Joey King')
+    john_carroll_lynch = Node.new('John Carroll Lynch')
 
-# Movie nodes
-crazy_stupid_love = Node.new('Crazy, Stupid, Love.')
+    # Movie nodes
+    crazy_stupid_love = Node.new('Crazy, Stupid, Love.')
 
-# Movie_list Class
-crazy_stupid_love = Movie_list.new(crazy_stupid_love, [john_carroll_lynch, joey_king, kevin_bacon, marisa_tomei])
+    # Movie_list Class
+    crazy_stupid_love = Movie_list.new(crazy_stupid_love, [john_carroll_lynch, joey_king, kevin_bacon, marisa_tomei])
 
-# Actor_list Class
-kevin_bacon = Actor_list.new(kevin_bacon, [crazy_stupid_love])
-marisa_tomei = Actor_list.new(marisa_tomei, [crazy_stupid_love])
-joey_king = Actor_list.new(joey_king, [crazy_stupid_love])
-john_carroll_lynch = Actor_list.new(john_carroll_lynch, [crazy_stupid_love])
+    # Actor_list Class
+    kevin_bacon = Actor_list.new(kevin_bacon, [crazy_stupid_love])
+    marisa_tomei = Actor_list.new(marisa_tomei, [crazy_stupid_love])
+    joey_king = Actor_list.new(joey_king, [crazy_stupid_love])
+    john_carroll_lynch = Actor_list.new(john_carroll_lynch, [crazy_stupid_love])
 
-puts "Movie_list Actor Nodes:"
-puts crazy_stupid_love.actors
+    puts 'Movie_list Actor Nodes:'
+    puts crazy_stupid_love.actors
 
-puts "Movie_list Movie Nodes:"
-puts crazy_stupid_love.movie
+    puts 'Movie_list Movie Nodes:'
+    puts crazy_stupid_love.movie
 
-puts "Actor_list Actor Nodes:"
-puts kevin_bacon.actor
+    puts 'Actor_list Actor Nodes:'
+    puts kevin_bacon.actor
 
-puts "Actor_list Movie Nodes:"
-puts kevin_bacon.movies
-
-# class Graph
-#     def traversal; end
-# end
-#
-#
-# crazy_stupid_love.film_hash['Crazy, Stupid, Love.'] = [john_carroll_lynch, joey_king, kevin_bacon, marisa_tomei]
+    puts 'Actor_list Movie Nodes:'
+    puts kevin_bacon.movies
+end
