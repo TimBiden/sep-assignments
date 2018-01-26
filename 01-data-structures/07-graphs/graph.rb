d9ebefa9b32163993944269f42f3f481cc72bee3
TimBiden <timbiden@gmail.com>
2018-01-26 12:45:21 -0800

    Instance Classes work, I think...

--- 01-data-structures/07-graphs/graph.rb
+++ 01-data-structures/07-graphs/graph.rb
@@ -5,6 +5,14 @@ class Movie_list
         @movie_name = movie_name
         @actor_array = actor_array
     end
+
+    def movie
+        @movie_name
+    end
+
+    def actors
+        @actor_array
+    end
 end
 
 class Actor_list
@@ -12,19 +20,49 @@ class Actor_list
         @actor_name = actor_name
         @movie_array = movie_array
     end
-end
 
-class Graph
-    def traversal; end
+    def actor
+        @actor_name
+    end
+
+    def movies
+        @movie_array
+    end
 end
 
 # Actor nodes
-kevin_bacon = Actor_node.new('Kevin Bacon')
-marisa_tomei = Actor_node.new('Marisa Tomei')
-joey_king = Actor_node.new('Joey King')
-john_carroll_lynch = Actor_node.new('John Carroll Lynch')
+kevin_bacon = Node.new('Kevin Bacon')
+marisa_tomei = Node.new('Marisa Tomei')
+joey_king = Node.new('Joey King')
+john_carroll_lynch = Node.new('John Carroll Lynch')
 
 # Movie nodes
-crazy_stupid_love = Movie_node.new('Crazy, Stupid, Love.')
+crazy_stupid_love = Node.new('Crazy, Stupid, Love.')
 
-crazy_stupid_love.film_hash['Crazy, Stupid, Love.'] = [john_carroll_lynch, joey_king, kevin_bacon, marisa_tomei]
+# Movie_list Class
+crazy_stupid_love = Movie_list.new(crazy_stupid_love, [john_carroll_lynch, joey_king, kevin_bacon, marisa_tomei])
+
+# Actor_list Class
+kevin_bacon = Actor_list.new(kevin_bacon, [crazy_stupid_love])
+marisa_tomei = Actor_list.new(marisa_tomei, [crazy_stupid_love])
+joey_king = Actor_list.new(joey_king, [crazy_stupid_love])
+john_carroll_lynch = Actor_list.new(john_carroll_lynch, [crazy_stupid_love])
+
+puts "Movie_list Actor Nodes:"
+puts crazy_stupid_love.actors
+
+puts "Movie_list Movie Nodes:"
+puts crazy_stupid_love.movie
+
+puts "Actor_list Actor Nodes:"
+puts kevin_bacon.actor
+
+puts "Actor_list Movie Nodes:"
+puts kevin_bacon.movies.data
+
+# class Graph
+#     def traversal; end
+# end
+#
+#
+# crazy_stupid_love.film_hash['Crazy, Stupid, Love.'] = [john_carroll_lynch, joey_king, kevin_bacon, marisa_tomei]
--- 01-data-structures/07-graphs/node.rb
+++ 01-data-structures/07-graphs/node.rb
@@ -1,27 +1,13 @@
-class Actor_node
-    attr_accessor :value
-    attr_accessor :adjacent_movies
+class Node
+  attr_accessor :value
+  attr_accessor :adjacent_nodes
 
-    def initialize(value)
-        @value = value
-        @adjacent_movies = []
-    end
+  def initialize(value)
+    @value = value
+    @adjacent_nodes = []
+  end
 
-    def add_edge(adjacent_node)
-        @adjacent_movies << adjacent_node
-    end
-end
-
-class Movie_node
-    attr_accessor :value
-    attr_accessor :adjacent_actors
-
-    def initialize(value)
-        @value = value
-        @adjacent_actors = []
-    end
-
-    def add_edge(adjacent_node)
-        @adjacent_actors << adjacent_node
-    end
+  def add_edge(adjacent_node)
+    @adjacent_nodes << adjacent_node
+  end
 end
