e56358b946ca9cdc9b967c4200c8ef7d4a484c73
TimBiden <timbiden@gmail.com>
2018-01-30 16:11:19 -0800

    Still doesn't work.

--- 01-data-structures/07-graphs/graph.rb
+++ 01-data-structures/07-graphs/graph.rb
@@ -1,74 +1,88 @@
 require_relative 'node'
 
 class Graph
-  def initialize
-    @nodes = {}
-  end
+    def initialize
+        @nodes = {}
+    end
 
-  def add_node(node)
-    @nodes[node] = node
-  end
+    def add_node(node)
+        @nodes[node] = node
+    end
 
-  def add_edge(node1, node2)
-    @nodes[node1].add_edge(@nodes[node2])
-    @nodes[node2].add_edge(@nodes[node1])
-  end
+    def add_movie_nodes(movies)
+        movies.each do |actor_key, movie_array|
+            length = movie_array.length - 1
 
-  def add_movies(movies)
-    movies.each do |key, array|
-      @movie_list = []
-      @movie_list << key
-      @actor_count[key] = array.length
-      length = array.length - 1
-
-      add_node(array[length])
-
-      until length == 0
-        nodeNum = length - 1
-        until nodeNum == -1
-          add_node(array[nodeNum])
-          add_edge(array[length], array[nodeNum])
-          nodeNum -= 1
+            until length == 0
+                add_node(movie_array[length])
+                length -= 1
+            end
         end
-        length -= 1
-      end
-    end
-  end
-
-  def traversal(root_node, search_value, movies)
-    # Add nodes & edges
-    initialize
-    add_node(root_node)
-    @actor_count = {}
-
-    add_movies(movies)
-
-    visited = []
-    to_visit = []
-
-    # add root node to visited list and to_visit queue
-    visited << root_node
-    to_visit << root_node
-
-    to_visit.each do |visitNode|
     end
 
-    until to_visit.empty?
-      current_node = to_visit.shift
+    def add_actor_nodes(actors)
+        actors.each do |movie_key, actor_array|
+            length = actor_array.length - 1
 
-      if current_node == search_value
-        # Node Found
-        return 'Found it!'
-      else
-        # Node Not Found - Searching deeper.
-        current_node.adjacent_nodes.each do |node|
-          unless visited.include?(node)
-            visited << node
-            to_visit << node
-          end
+            until length == 0
+                add_node(actor_array[length])
+                length -= 1
+            end
         end
-      end
     end
-    'Not found!'
-  end
+
+    def add_edge(node1, node2)
+        @nodes[node1].add_edge(@nodes[node2])
+        @nodes[node2].add_edge(@nodes[node1])
+    end
+
+    def add_edges_from_actors(actors)
+        actors.each do |movie_key, actor_array|
+            puts ' '
+            puts "movie_key = #{movie_key}"
+            actor_array.each do|actor|
+                puts "actor node = #{actor}"
+                puts "actor value = #{actor.value}"
+            end
+        end
+    end
+
+    def traversal(root_node, search_value, movies, actors)
+        # Add nodes & edges
+        initialize
+        add_node(root_node)
+        @actor_count = {}
+
+        add_movie_nodes(movies)
+        add_actor_nodes(actors)
+        add_edges_from_actors(actors)
+
+        visited = []
+        to_visit = []
+
+        # add root node to visited list and to_visit queue
+        visited << root_node
+        to_visit << root_node
+
+        to_visit.each do |visitNode|
+        end
+
+        until to_visit.empty?
+            current_node = to_visit.shift
+
+            if current_node == search_value
+                # Node Found
+                return 'Found it!'
+            else
+                # Node Not Found - Searching deeper.
+                current_node.adjacent_nodes.each do |node|
+                    unless visited.include?(node)
+                        visited << node
+                        to_visit << node
+                    end
+                end
+            end
+        end
+        'Not found!'
+    end
 end
